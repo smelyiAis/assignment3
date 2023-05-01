1b0d8ef8ae931d96e260a950fda133222272c75e
smelyiAis <124904635+smelyiAis@users.noreply.github.com>
2023-05-01 22:18:01 +0600

    Add files via upload

---
+++ MyHashTable.java
@@ -0,0 +1,26 @@
+public class MyHashTable<K, V> {
+    private class HashNode<K,V>{
+        private K key;
+        private V value;
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K,V>[] chainArray;
+    private int M = 11;
+    private int size;
+    public MyHashTable(){}
+    public MyHashTable(int M){}
+    private int hash(K key){return 0;}
+    public void put(K key, V value){}
+    public V get(K key){}
+    public V remove(K key){}
+    public boolean contains(V value){}
+    public K getKey(V value){}
+
+}
